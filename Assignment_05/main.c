926b1e64ecb65f6a3d015578d808e847d56bc7e8
Guillaume <guillaume2.chopin@orange.fr>
2024-05-14 18:26:04 +0200

    maj du 14/05

--- Assignment_05/main.c
+++ Assignment_05/main.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/string.h>
 #include <linux/miscdevice.h>
 
 MODULE_LICENSE("42");
@@ -8,23 +9,26 @@ MODULE_AUTHOR("gchopin");
 
 struct miscdevice g_misc;
 
-static ssize_t ft_read(struct file *rd,  char __user * u, size_t s_val, loff_t *loff) {
+static ssize_t ft_read(struct file *rd,  char __user * u, size_t count, loff_t *loff) {
 	return (0);
 }
 
-static ssize_t ft_write(struct file *wr, const char __user * u, size_t s_val, loff_t *loff) {
+static ssize_t ft_write(struct file *wr, const char __user * u, size_t count, loff_t *loff) {
 	return (0);
 }
+
 /*
  * To register a device with a minor number
  * https://www.kernel.org/doc/html/v4.13/driver-api/misc_devices.html
 */
 static int __init init_hello(void)
 {
-	struct file_operations fops;
-	fops.owner = THIS_MODULE;
-	fops.read = &ft_read;
-	fops.write = &ft_write;
+	struct file_operations fops = {
+		.owner = THIS_MODULE,
+		.read = ft_read,
+		.write = ft_write
+	};
+	memset(&g_misc, 0, sizeof(struct miscdevice));
 	g_misc.fops = &fops;
 	g_misc.minor = MISC_DYNAMIC_MINOR;
 	g_misc.name = "c_hello_world";
